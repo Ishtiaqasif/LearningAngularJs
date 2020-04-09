a52aa972c34e081265bebec14fb36d9b90159054
Asif Ishtiaq <ishtiaqasif03@gmail.com>
2020-04-09 11:15:43 +0600

    Login App 1 & Login App 2

---
+++ Scripts/loginapp2.js
@@ -0,0 +1,40 @@
+let app = angular.module('login-app', ['ngRoute']);
+app.config($routeProvider => {
+    $routeProvider
+    .when('/',{
+        templateUrl: 'login.html'
+    })
+    .when('/dashboard',{
+        resolve:{
+            'check': ($location, $rootScope) => {
+                if (!$rootScope.loggedIn) {
+                    $location.path('/');
+                }
+            }
+        },
+        templateUrl: 'dashboard.html'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+});
+
+
+app.controller('loginController',($scope, $location, $rootScope) => {
+    $scope.submit = () => {
+        let username = $scope.username;
+        let password = $scope.password;
+
+        if(username == 'admin' && password == 'admin'){
+            $rootScope.loggedIn = true;
+            $location.path('/dashboard');             
+        }
+        else{
+            alert('Authentication Failed');
+        }
+    };
+    $scope.logout = () => {
+        $rootScope.loggedIn = false;
+            $location.path('/');  
+    };
+});
