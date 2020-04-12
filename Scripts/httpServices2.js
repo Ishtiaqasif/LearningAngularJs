dc97215348040abe4adb65223e74f82c2a810bd7
Asif Ishtiaq <ishtiaqasif03@gmail.com>
2020-04-12 16:19:16 +0600

    Advance Search Box With Table

---
+++ Scripts/httpServices2.js
@@ -0,0 +1,9 @@
+let appl = angular.module('http-app2', []);
+appl.controller('men', ($http, $scope) => {
+    $http.get('http://127.0.0.1:5500/db/db.json')
+    .then(response => {
+        $scope.friends = response.data.friends;
+      }, response => {
+        console.log(response.statusText);
+      });
+});
