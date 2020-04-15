82066723b785ec07b48e57674c2141b79e4c5ac9
Asif Ishtiaq <ishtiaqasif03@gmail.com>
2020-04-15 14:37:36 +0600

    Services

---
+++ Scripts/services.js
@@ -0,0 +1,13 @@
+let app = angular.module('services-app',[]);
+
+app.service('randNumGenerator', function() {
+    let randNum = Math.ceil(Math.random()*100);
+    this.generate = () => randNum;
+});
+
+app.controller('servicesCtrlr', ($scope, randNumGenerator) => {
+
+    $scope.generateRandom = () =>{
+        $scope.randomNumber = randNumGenerator.generate();
+    };
+});
